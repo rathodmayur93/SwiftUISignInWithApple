cecadde01b2a25de5fcfd1b96c0f0f97f09e3688
Mayur Rathod <rathodmayur93@gmail.com>
2019-12-08 07:26:15 +0300

    Initial Commit

---
+++ AppleSignUp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleSignUp
+//
+//  Created by ds-mayur on 12/8/19.
+//  Copyright © 2019 Mayur Rathod. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
